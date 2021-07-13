596666900b3e4e62c4106ff034d83d86bef7cace
Railgun <Adhibits@protonmail.com>
2021-07-14 00:12:00 +0200

    Add correct smart contracts

--- app/src/drizzleOptions.ts
+++ app/src/drizzleOptions.ts
@@ -1,17 +1,17 @@
-import Web3 from "web3";
-import ComplexStorage from "./contracts/ComplexStorage.json";
-import SimpleStorage from "./contracts/SimpleStorage.json";
-import TutorialToken from "./contracts/TutorialToken.json";
+import Web3 from "web3"
+import piggyGameWithNFT from "./contracts/piggyGame.json"
+import profileNFT from "./contracts/WBProfileNFT.json"
+import rewardNFT from "./contracts/boosterNFT.json"
 
 const options = {
   web3: {
     block: false,
     customProvider: new Web3("ws://localhost:8545"),
   },
-  contracts: [SimpleStorage, ComplexStorage, TutorialToken],
-  events: {
-    SimpleStorage: ["StorageSet"],
-  },
-};
+  contracts: [piggyGameWithNFT, profileNFT, rewardNFT],
+  // events: {
+  //   SimpleStorage: ["StorageSet"],
+  // },
+}
 
-export default options;
+export default options
