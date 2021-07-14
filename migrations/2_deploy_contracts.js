bb9a9faaf1565499c43d426a7ad17987cf94a6f5
Railgun <Adhibits@protonmail.com>
2021-07-14 14:10:06 +0200

    Point to real contract address

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,10 +4,9 @@ const boosterNFT = artifacts.require("boosterNFT")
 const PiggyBankToken = artifacts.require("PiggyBankToken")
 
 module.exports = function(deployer) {
-  deployer.deploy(PiggyBankToken).then(() => {
-    deployer.deploy(profileNFT)
-    deployer.deploy(boosterNFT).then(() => {
-      deployer.deploy(piggyGame, PiggyBankToken.address, boosterNFT.address)
+    return deployer.deploy(profileNFT).then(() => {
+      return deployer.deploy(boosterNFT).then(() => {
+        return deployer.deploy(piggyGame, "0xcd2ecd5e06b1a330789b30e8ada3d11c51503a71", boosterNFT.address)
     })
   })
 }
