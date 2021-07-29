1e9f0e8829a6ca49e1aa3ce72334c20b8be95d63
Railgun <Adhibits@protonmail.com>
2021-07-30 00:39:40 +0200

    Piggy Game

--- test/piggyGame.js
+++ test/piggyGame.js
@@ -8,11 +8,10 @@ contract("PiggyGame", accounts => {
     const balance = await game.balanceOf.call(accounts[0])
     assert.equal(balance, 0, "Balance not zero")
     await game.updateNFTAddress(nft.address)
-    await game.setOpen(true)
     await game.updatePancakeSwapRouter("0x10ed43c718714eb63d5aa57b78b54704e256024e", '0xcd2ecd5e06b1a330789b30e8ada3d11c51503a71')
     await game.addTeam()
     await game.addTeam()
-    await game.setSeason(1)
+    await game.openSeason();
     await game.join({ from: accounts[0] , value: "100000000000000000"})
     await game.mintNFT(accounts[0], 5, 6);
     await game.transferOwnership(accounts[1])
@@ -28,7 +27,9 @@ contract("PiggyGame", accounts => {
     await game.withdraw("42500000212500001")
     const balanceFinal = await game.balanceOf.call(accounts[0])
     assert.equal(balanceFinal, 0, "Balance not zero")
-    await game.buyTokens("5000000000000000000", { from: accounts[0] , value: "500000000000000000"})
-    await game.attack("50000000000000000", 2)
+    
+    await game.buyTokens("1500000000000000000000", { from: accounts[0] , value: "50000000000000000000"})
+    const res = await game.attack("1000000000000000000000", 2)
+    console.log(res.logs)
   })
 })
