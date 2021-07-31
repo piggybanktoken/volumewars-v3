3980837447f9f823f6f0c7edb8875eae656e586c
Railgun <Adhibits@protonmail.com>
2021-07-31 04:35:04 +0200

    Remove dead code

--- app/src/components/war.tsx
+++ app/src/components/war.tsx
@@ -51,26 +51,8 @@ export function War() {
 
     const dispatch = useAppDispatch()
 
-    enum AttackState {
-        Closed = 1,
-        Confirm,
-        Wait
-    }
-    const [attackSize, setSize] = useState(0)
-    const [attackTarget, setTarget] = useState(0)
-    const [modalState, setModalState] = useState(AttackState.Closed)
     const teams = [1, 2, 3, 4, 5]
     const nteams = 5
-
-    function startAttack(team: number) {
-        setTarget(team)
-        setModalState(AttackState.Confirm)
-    }
-
-    function confirmAttack() {
-        // TODO stub
-      setModalState(AttackState.Wait)
-    }
 
     return (
         <div>
@@ -98,58 +80,12 @@ export function War() {
                                     <Button fluid negative onClick={() => dispatch(openAttackModal(tnumber.toString()))}>Attack!</Button>
                                 </Grid.Column>
                             )}
-
                         </Grid>
                     </Segment>
 
                 </Grid.Column>
             </Grid>
             <AttackModal />
-            <Modal
-                onClose={() => setModalState(AttackState.Closed)}
-                onOpen={() => setModalState(AttackState.Confirm)}
-                open={modalState != AttackState.Closed}
-            >
-                <Modal.Header>Attack This Team with {attackSize} PIGGY?</Modal.Header>
-                <Modal.Content>
-                    {(modalState == AttackState.Confirm) &&
-                        <Grid columns={3}>
-                        <Grid.Column/>
-                        <Grid.Column textAlign="center">
-                        <RenderTeam team={attackTarget} />
-                        </Grid.Column>
-                        </Grid>
-                    }
-                    {(modalState == AttackState.Wait) &&
-                        <p> Attack started! We will notify you when the attack finishes. </p>
-                    }
-                </Modal.Content>
-                <Modal.Actions>
-                    {(modalState == AttackState.Confirm) &&
-                        <Button color='black' onClick={() => setModalState(AttackState.Closed)}>
-                            Cancel
-                     </Button>
-                    }
-                    {(modalState == AttackState.Confirm) &&
-                        <Button
-                            content="ATTACK!"
-                            labelPosition='right'
-                            icon='checkmark'
-                            onClick={confirmAttack}
-                            positive
-                        />
-                    }
-                    {(modalState == AttackState.Wait) &&
-                        <Button
-                            content="Finish."
-                            labelPosition='right'
-                            icon='checkmark'
-                            onClick={() => setModalState(AttackState.Closed)}
-                            positive
-                        />
-                    }
-                </Modal.Actions>
-            </Modal>
         </div>
     )
 }
