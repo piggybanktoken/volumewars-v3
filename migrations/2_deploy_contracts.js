50009adb416a9a863c770f6161a15e1a71d59161
Railgun <Adhibits@protonmail.com>
2021-08-02 01:59:54 +0200

    Testnet link parameters

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -12,9 +12,9 @@ const MAINNET_LINK = {
   fee: "200000000000000000"
 }
 const TESTNET_LINK = {
+  token: "0x84b9B910527Ad5C03A9Ca831909E21e236EA7b06",
   coordinator: "0xa555fC018435bef5A13C6c6870a9d4C11DEC329C",
   hash: "0xcaf3c3727e033261d383b315559476f48034c13b18f8cafed4d871abe5049186",
-  token: "0x84b9B910527Ad5C03A9Ca831909E21e236EA7b06",
   fee: "100000000000000000"
 }
 module.exports = async (deployer, network, [defaultAccount]) => {
@@ -23,7 +23,7 @@ module.exports = async (deployer, network, [defaultAccount]) => {
       return deployer.deploy(rewardNFT, piggyGame.address)
     })
   }
-  return deployer.deploy(piggyGame, TESTNET_PIGGY, TESTNET_PCS, MAINNET_LINK.coordinator, MAINNET_LINK.token, MAINNET_LINK.hash, MAINNET_LINK.fee).then(() => {
+  return deployer.deploy(piggyGame, TESTNET_PIGGY, TESTNET_PCS, TESTNET_LINK.coordinator, TESTNET_LINK.token, TESTNET_LINK.hash, TESTNET_LINK.fee).then(() => {
     return deployer.deploy(rewardNFT, piggyGame.address)
   })
   
