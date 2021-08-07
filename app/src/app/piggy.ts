5fa9257cbee888994015bac94f9416d41e4e9b88
Railgun <Adhibits@protonmail.com>
2021-08-07 12:16:39 +0200

    Generic token address

--- app/src/app/piggy.ts
+++ app/src/app/piggy.ts
@@ -1,8 +1,7 @@
 import { ChainId, Token } from '@pancakeswap/sdk'
 import { ethers } from "ethers"
-import { Web3Provider, JsonRpcProvider } from '@ethersproject/providers'
+import { Web3Provider } from '@ethersproject/providers'
 import { Contract } from '@ethersproject/contracts'
-import Web3 from 'web3'
 
 export const tokenAddress = '0xcd2ecd5e06b1a330789b30e8ada3d11c51503a71'
 const TESTNET_PIGGY = "0xd610e8523b335e6f3cebf1bd564800b755eafdfe"
@@ -10,11 +9,11 @@ const TESTNET_PIGGY = "0xd610e8523b335e6f3cebf1bd564800b755eafdfe"
 const decimals = 9
 export const PIGGY = new Token(ChainId.TESTNET, TESTNET_PIGGY, decimals, "PIGGY")
 
-function getContract(): ethers.Contract {
+function getContract(address: string): ethers.Contract {
     const wprovider = new Web3Provider((window as any).ethereum as any,  {name: "binance", chainId: 97 })
     const signer = wprovider.getSigner()
     return new Contract(
-        TESTNET_PIGGY,
+        address,
         [
             'function balanceOf(address account) external view returns (uint256)',
             'function transfer(address recipient, uint256 amount) external returns (bool)',
@@ -26,16 +25,16 @@ function getContract(): ethers.Contract {
     )
 }
 
-export async function balanceOf(account: string): Promise<string> {
-    const piggy = getContract()
+export async function balanceOf(account: string, tokenAddress: string): Promise<string> {
+    const piggy = getContract(tokenAddress)
     return (await piggy.balanceOf(account)).toString()
 }
 
-export async function approve(spender: string, amount: string): Promise<boolean> {
-    const piggy = getContract()
+export async function approve(spender: string,  amount: string, tokenAddress: string): Promise<boolean> {
+    const piggy = getContract(tokenAddress)
     return await piggy.approve(spender, amount)
 }
-export async function transfer(recipient: string, amount: string): Promise<boolean> {
-    const piggy = getContract()
+export async function transfer(recipient: string, amount: string, tokenAddress: string): Promise<boolean> {
+    const piggy = getContract(tokenAddress)
     return await piggy.transfer(recipient, amount)
 }
