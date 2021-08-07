affc5778bffc60edbeb8bea0b41e13a330186cc2
Railgun <Adhibits@protonmail.com>
2021-08-07 12:16:20 +0200

    Generic base unit conversion

--- app/src/app/utils.ts
+++ app/src/app/utils.ts
@@ -8,3 +8,12 @@ export function baseUnitsToPiggy(piggyNumber: string): string {
     if (!piggyNumber) return "0"
     return new BN(piggyNumber).div(new BN(10**9)).toString()
 }
+
+export function tokenToBaseUnits(amount: string, decimals: string): string {
+    if (!amount || !decimals) return "0"
+    return new BN(amount).mul(new BN(decimals)).toString()
+}
+export function baseUnitsToTokens(amount: string, decimals: string): string {
+    if (!amount || !decimals) return "0"
+    return new BN(amount).div(new BN(decimals)).toString()
+}
